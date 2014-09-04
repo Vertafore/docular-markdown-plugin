4ce58b5e6427b9a69f0d779e8328c8ce57a4918b
Stephen Liberty <stephen@liberty-irm.com>
2014-09-04 14:37:39 -0700

    Moving markdown rendering up a level

--- web/markdownPlugin.js
+++ web/markdownPlugin.js
@@ -1,4 +1,4 @@
 angular.module('docular.plugin.markdown', [])
-    .controller('docular.plugin.markdown.documentationController', ['$scope', 'markdown', function ($scope, markdownService) {
-        $scope.content = markdownService($scope.documentationItem.content);
+    .controller('docular.plugin.markdown.documentationController', ['$scope', function ($scope) {
+        $scope.content = $scope.documentationItem.content;
     }]);
